44939e32f3e15a5429b0095e6b03aef1268c7eda
oof2000 <olivierkoelemij@Oliviers-MacBook-Pro.local>
2017-07-20 10:39:25 -0700

    Core build from 1997

---
+++ test/index.php
@@ -0,0 +1,155 @@
+<?php 
+
+//VARS
+
+$title 				= 'Powerplay People';
+$body_title 		= 'Distributie & Logisitiek';
+$body_text			= '<h3>Magnis dis parturient montes</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae velit at velit pretium sodales. 
+						Maecenas egestas imperdiet mauris, vel elementum turpis iaculis eu. Duis egestas augue quis ante ornare 
+						eu tincidunt magna interdum. Vestibulum posuere risus non ipsum sollicitudin quis viverra ante feugiat. 
+						Pellentesque non faucibus lorem. Nunc tincidunt diam nec risus ornare quis porttitor enim pretium. 
+						Ut adipiscing tempor massa, a ullamcorper massa bibendum at. Suspendisse potenti. In vestibulum enim 
+						orci, nec consequat turpis. Suspendisse sit amet tellus a quam volutpat porta. Mauris ornare augue ut 
+						diam tincidunt elementum. Vivamus commodo dapibus est, a gravida lorem pharetra eu. Maecenas ultrices
+						cursus tellus sed congue. Cras nec nulla erat.</p>
+						
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque eget 
+						mauris libero. Nulla sit amet felis in sem posuere laoreet ut quis elit. Aenean mauris massa, pretium non 
+						bibendum eget, elementum sed nibh. Nulla ac felis et purus adipiscing rutrum. Pellentesque a bibendum sapien. 
+						Vivamus erat quam, gravida sed ultricies ac, scelerisque sed velit. Integer mollis urna sit amet ligula aliquam 
+						ac sodales arcu euismod. Fusce fermentum augue in nulla cursus non fermentum lorem semper. Quisque eu auctor lacus. 
+						Donec justo justo, mollis vel tempor vitae, consequat eget velit.</p>
+						
+						<p>Vivamus sed tellus quis orci dignissim scelerisque nec vitae est. Duis et elit ipsum. Aliquam pharetra auctor 
+						felis tempus tempor. Vivamus turpis dui, sollicitudin eget rhoncus in, luctus vel felis. Curabitur ultricies 
+						dictum justo at luctus. Nullam et quam et massa eleifend sollicitudin. Nulla mauris purus, sagittis id egestas 
+						eu, pellentesque et mi. Donec bibendum cursus nisi eget consequat. Nunc sit amet commodo metus. Integer 
+						consectetur lacus ac libero adipiscing ut tristique est viverra. Maecenas quam nibh, molestie nec pretium 
+						interdum, porta vitae magna. Maecenas at ligula eget neque imperdiet faucibus malesuada sed ipsum. Nulla auctor
+						ligula sed nisl adipiscing vulputate. Curabitur ut ligula sed velit pharetra fringilla. Cras eu luctus est. 
+						Aliquam ac urna dui, eu rhoncus nibh. Nam id leo nisi, vel viverra nunc. Duis egestas pellentesque lectus, a 
+						placerat dolor egestas in. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+						Donec vitae ipsum non est iaculis suscipit.</p>
+						
+						<div class="gallery">
+							<a rel="example_group" href="images/test.jpg" title="Supergave foto nummer 1"><img src="images/test.jpg" /></a>
+							<a rel="example_group" href="images/test.jpg" title="Supergave foto nummer 2"><img src="images/test.jpg" /></a>
+							<a rel="example_group" href="images/test.jpg" title="Supergave foto nummer 3"><img src="images/test.jpg" /></a>
+						</div>
+						
+						<h3>Adipiscing risus </h3>
+						<p>Quisque vel felis ligula. Cras viverra sapien auctor ante porta a tincidunt quam pulvinar. Nunc facilisis, enim 
+						id volutpat sodales, leo ipsum accumsan diam, eu adipiscing risus nisi eu quam. Ut in tortor vitae elit condimentum 
+						posuere vel et erat. Duis at fringilla dolor. Vivamus sem tellus, porttitor non imperdiet et, rutrum id nisl. Nunc 
+						semper facilisis porta. Curabitur ornare metus nec sapien molestie in mattis lorem ullamcorper. Ut congue, purus ac 
+						suscipit suscipit, magna diam sodales metus, tincidunt imperdiet diam odio non diam. Ut mollis lobortis vulputate. 
+						Nam tortor tortor, dictum sit amet porttitor sit amet, faucibus eu sem. Curabitur aliquam nisl sed est semper a 
+						fringilla velit porta. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum 
+						in sapien id nulla volutpat sodales ac bibendum magna. Cras sollicitudin, massa at sodales sodales, lacus tortor vestibulum massa, 
+						eu consequat dui nulla et ipsum.</p>
+						
+						<p>Aliquam accumsan aliquam urna, id vulputate ante posuere eu. Nullam pretium convallis tincidunt. Duis vitae odio arcu, ut
+						fringilla enim. Nam ante eros, vestibulum sit amet rhoncus et, vehicula quis tellus. Curabitur sed iaculis odio. Praesent 
+						vitae ligula id tortor ornare luctus. Integer placerat urna non ligula sollicitudin vestibulum. Nunc vestibulum auctor massa, 
+						at varius nibh scelerisque eget. Aliquam convallis, nunc non laoreet mollis, neque est mattis nisl, nec accumsan velit nunc ut 
+						arcu. Donec quis est mauris, eu auctor nulla. Fusce leo diam, tempus a varius sit amet, auctor ac metus. Nam turpis nulla, 
+						fermentum in rhoncus et, auctor id sem. Aliquam id libero eu neque elementum lobortis nec et odio.</p>                   
+					
+						<iframe width="370" height="240" src="http://www.youtube.com/embed/sEhy-RXkNo0?rel=0" frameborder="0" allowfullscreen></iframe>
+						
+						<p>Aliquam accumsan aliquam urna, id vulputate ante posuere eu. Nullam pretium convallis tincidunt.
+						Duis vitae odio arcu, ut fringilla enim. Nam ante eros, vestibulum sit amet rhoncus et, vehicula quis tellus. Curabitur 
+						sed iaculis odio. Praesent vitae ligula id tortor ornare luctus. Integer placerat urna non ligula sollicitudin vestibulum.
+						Nunc vestibulum auctor massa, at varius nibh scelerisque eget. Aliquam convallis, nunc non laoreet mollis, neque est mattis nisl, 
+						nec accumsan velit nunc ut arcu. Donec quis est mauris, eu auctor nulla. Fusce leo diam, tempus a varius sit amet, auctor ac metus. 
+						Nam turpis nulla, fermentum in rhoncus et, auctor id sem. Aliquam id libero eu neque elementum lobortis nec et odio.</p> '
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+	<!-- Meta -->
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	
+	<!-- Title -->
+	<title><?php echo $title; ?></title>
+	
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/main.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./fancybox/jquery.fancybox-1.3.4.css" media="screen" />	
+	
+	<!-- JS -->
+	<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+	<script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+	<script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.tinyscrollbar.min.js"></script>
+	<script type="text/javascript" src="js/scrollbar.js"></script>	
+	<script type="text/javascript" src="js/lightbox.js"></script>
+</head>
+
+<body>
+	<!-- BG IMAGE -->
+	<div id="bg_image">
+		<img id="background" src="images/adam.jpg" alt="" title="">		
+	</div>
+
+	<!-- CONTAINER -->
+	<div id="IE_body">
+		<div id="container">
+		
+			<!-- Logo -->
+			<div id="logo">
+				<?php require('required/logo.html') ;?>
+			</div>
+			
+			<!-- Social Media -->
+			<div id="social">
+				<ul>
+					<li id="fb"><a href="http://www.facebook.com/pages/Powerplay-People/222677637755563" target="_blank"></a></li>
+					<li id="tw"><a href="http://twitter.com/#!/powerplaypeople" target="_blank"></a></li>
+					<li id="li"><a href="http://www.linkedin.com/company/powerplay-people" target="_blank"></a></li>
+				</ul>	
+			</div>
+			
+			<!-- Menu left -->
+			<div id="menu_left">
+				<?php require('required/menu_left.html') ;?>
+			</div>
+				
+			<!-- Body Content -->	
+			<div id="body_content">
+				<h1><?php echo $body_title; ?></h1>
+				<div id="body_content_input">
+					<div id="scrollbar1">
+						<div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>
+						<div class="viewport">
+							 <div class="overview">
+								<?php echo $body_text; ?>     							
+							</div>
+						</div>
+					</div>	
+				</div>	
+			</div>
+
+			<!-- Menu bottom -->
+			<div id="menu_bottom">
+				<?php require('required/menu_bottom.html') ;?>
+			</div>		
+			
+			
+			<!-- Wieber Cut -->
+			<div class="wieber_cut">
+				&nbsp;
+			</div>		
+			
+			<!-- Wieber -->
+			<div class="wieber">
+				&nbsp;
+			</div>
+		</div>	
+	</div>
+
+	
+</body>	
+</html>
---
+++ test/js/scrollbar.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+			
+	$('#scrollbar1').tinyscrollbar();
+	
+});
